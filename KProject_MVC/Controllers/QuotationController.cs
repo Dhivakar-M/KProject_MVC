2a767089f3eaff4504f13913eebc59293f1850cc
Dhivakar M <dhivakarworks@outlook.com>
2022-07-13 08:24:08 +0530

    Quotation UI design done

---
+++ KProject_MVC/Controllers/QuotationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KProject_MVC.Controllers
+{
+    public class QuotationController : Controller
+    {
+        // GET: Quotation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
